c76851007735bcacc58226d5dfa1089926e590e8
myukang <myunghwan0421@gmail.com>
2022-11-17 22:03:21 +0900

    FEAT: 로그인 정보에 구분 문자열 추가 header include 삭제

--- src/dbs/dblogin/MariaLogin.js
+++ src/dbs/dblogin/MariaLogin.js
@@ -2,11 +2,11 @@ const mariadb = require('mariadb');
 
 export default async function MariaMyLogin(config, query) {
 	const conn = await mariadb.createConnection(config);
-	console.log(config)
 	if (query)
 	{
 		try {
 			const res = await conn.query(query);
+			console.log("get a result in query:", query, "res: ", res);
 			return (res);
 		} catch(e){
 			console.log(e)
