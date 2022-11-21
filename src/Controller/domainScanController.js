369ce64b880be28222eba3cc6b634f12ff0c8959
myukang <myunghwan0421@gmail.com>
2022-11-21 19:59:04 +0900

    FEAT: 정리

--- src/Controller/domainScanController.js
+++ src/Controller/domainScanController.js
@@ -1,6 +1,5 @@
 import getTableNames from "./tools/domainScan/getTableNames";
-import getNumeric from "./tools/domainScan/getNumeric";
-import getCategory from "./tools/domainScan/getCatgory";
+import { ScanResult } from "./tools/domainScan/ScanResult";
 
 export const getDomainScan = async (req, res) => {
 	const loginInfo = req.session.loginInfo;
@@ -15,14 +14,18 @@ export const getDomainScan = async (req, res) => {
 
 export const getDomainScanResult = async (req, res) => {
 	console.log("routing domain-test")
-	const { id } = req.params;
+	const { tableName } = req.params;
 	const { loginInfo } = req.session;
+	console.log(tableName);
 	try {
-		const numericFields = getNumeric(id, loginInfo);
-		const categoryFields = getCategory(id, loginInfo);
-		res.render("domain-scan-result",  { title : "PRESWOT LAB" });
+		let ScanObject = new ScanResult(tableName, loginInfo);
+		const result = await ScanObject.getResult();
+		console.log("NUMERIC SCAN RESULT : ", result.numericResult, "\n");
+		console.log("CATEGORY SCAN RESULT : ", result.categoryResult, "\n");
+
+
+		res.render("domain-scan-result",  { title : "PRESWOT LAB", result});
 	} catch (e) {
-		console.log(e);
 		res.status(404).redirect('/logout');
 	}
 }
