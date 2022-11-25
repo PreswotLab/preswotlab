074d310434a22f865035c20e507a296b6212e29f
myukang <myunghwan0421@gmail.com>
2022-11-25 17:26:33 +0900

    FEAT: console.log 삭제

--- src/env.js
+++ src/env.js
@@ -1,7 +1,4 @@
 //  importing resolve from path module 
 const {resolve} = require('path');
 
-console.log(resolve(__dirname,"../.env"))
 require('dotenv').config({path: resolve(__dirname,"../.env")})
-
-console.log(process.env);
