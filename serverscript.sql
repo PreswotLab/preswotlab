69cdccf54992c41d4f495fb4ae0d826e7943bf60
myukang <myunghwan0421@gmail.com>
2022-11-25 15:34:26 +0900

    FEAT: 서버 스크립트 수정

--- serverscript.sql
+++ serverscript.sql
@@ -10,16 +10,12 @@ FLUSH PRIVILEGES;
 
 -- 사용자
 CREATE TABLE tb_user (
-	user_seq CHAR(36) NOT NULL DEFAULT UUID(), 
+	user_seq INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	db_type VARCHAR(10) NOT NULL, 
 	host VARCHAR(100) NOT NULL, 
 	port VARCHAR(5) NOT NULL,
 	user_id VARCHAR(20),
 	db_name VARCHAR(20) NOT NULL
-);
-
-ALTER TABLE tb_user ADD CONSTRAINT PK_TB_USER PRIMARY KEY (
-	user_seq
 );
 
 ALTER TABLE tb_user ADD CONSTRAINT UNIQUE_TB_USER UNIQUE(
@@ -31,14 +27,15 @@ ALTER TABLE tb_user ADD CONSTRAINT UNIQUE_TB_USER UNIQUE(
 
 -- 스캔 테이블
 CREATE TABLE tb_scan (
-	table_seq INT NOT NULL,
+	table_seq INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	user_seq INT NOT NULL, 
 	table_name VARCHAR(50) NOT NULL,
 	scan_yn enum('Y','N')NOT NULL DEFAULT 'N'
 );
 
-ALTER TABLE tb_scan ADD CONSTRAINT PK_TB_SCAN PRIMARY KEY (
-	table_seq
+ALTER TABLE tb_scan ADD CONSTRAINT UNIQUE_TB_SCAN UNIQUE (
+	user_seq,
+	table_name
 );
 
 ALTER TABLE tb_scan ADD CONSTRAINT FK_tb_user_TO_tb_scan_1 FOREIGN KEY (
@@ -49,27 +46,27 @@ ALTER TABLE tb_scan ADD CONSTRAINT FK_tb_user_TO_tb_scan_1 FOREIGN KEY (
 
 -- 대표 속성 사전
 CREATE TABLE tb_rep_attribute (
-	rattr_seq INT NOT NULL,
+	rattr_seq INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	rattr_name VARCHAR(50) NOT NULL
 );
 
-ALTER TABLE tb_rep_attribute ADD CONSTRAINT PK_TB_REP_ATTRIBUTE PRIMARY KEY (
-	rattr_seq
+ALTER TABLE tb_rep_attribute ADD CONSTRAINT UNIQUE_TB_REP_ATTRIBUTE UNIQUE (
+	rattr_name
 );
 
 -- 대표 결합키 사전
 CREATE TABLE tb_rep_key ( 
-	rkey_seq INT NOT NULL,
+	rkey_seq INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	rkey_name VARCHAR(50) NOT NULL
-); 
+);
 
-ALTER TABLE tb_rep_key ADD CONSTRAINT PK_TB_REP_KEY PRIMARY KEY (
-	rkey_seq
+ALTER TABLE tb_rep_key ADD CONSTRAINT UNIQUE_TB_REP_KEY UNIQUE (
+	rkey_name
 );
 
 -- 속성
 CREATE TABLE tb_attribute (
-	attr_seq INT NOT NULL, 
+	attr_seq INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	table_seq INT NOT NULL, 
 	attr_name VARCHAR(50) NOT NULL, 
 	attr_type enum('N', 'C') NOT NULL, 
@@ -84,9 +81,9 @@ CREATE TABLE tb_attribute (
 	key_candidate enum('Y', 'N') NOT NULL DEFAULT 'N'
 );
 
-alter table tb_attribute add constraint pk_tb_attribute primary key (
-	attr_seq,
-	table_seq
+alter table tb_attribute add constraint unique_tb_attribute UNIQUE (
+	table_seq,
+	attr_name
 );
 
 alter table tb_attribute add constraint fk_tb_scan_to_tb_attribute_1 foreign key (
@@ -103,15 +100,14 @@ alter table tb_attribute add constraint fk_tb_rep_attribute_to_tb_attribute_1 fo
 
 -- 대표 결합키 매핑
 CREATE TABLE tb_mapping (
-	mapping_seq INT NOT NULL, 
+	mapping_seq INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
 	rkey_seq INT NOT NULL, 
 	attr_seq INT NOT NULL, 
 	table_seq INT NOT NULL,
 	chg_yn enum('Y','N') NOT NULL DEFAULT 'N'
 );
 
-ALTER TABLE tb_mapping ADD CONSTRAINT PK_TB_MAPPING PRIMARY KEY (
-	mapping_seq,
+ALTER TABLE tb_mapping ADD CONSTRAINT UNIQUE_TB_MAPPING UNIQUE (
 	rkey_seq,
 	attr_seq,
 	table_seq
@@ -134,7 +130,6 @@ ALTER TABLE tb_mapping ADD CONSTRAINT FK_tb_attribute_TO_tb_mapping_2 FOREIGN KE
 ) REFERENCES tb_attribute ( 
 	table_seq 
 ); 
-
 
 -- 조인결합결과
 CREATE TABLE tb_join ( 
