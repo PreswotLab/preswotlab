efd0b1a0eb0edd7c9cfe8e92f072d1fc6b0191dc
myukang <myunghwan0421@gmail.com>
2022-11-28 02:39:14 +0900

    FEAT: 입력받지 않으면 name = null이므로, e.target.value를 -로 바꾼다.

--- assets/js/domain-scan-result.js
+++ assets/js/domain-scan-result.js
@@ -2,22 +2,25 @@ import { postData } from "./postData.js";
 
 const addRepOptions = document.getElementsByClassName("repSelect");
 
-const addRepAttr = async () => {
+const addRepAttr = async (e) => {
 	const name = prompt ("추가하고자하는 대표 속성을 입력하세요");
-	console.log(name);
-	if (name)
+	if (!name)
+		e.target.value = "-";
+	if (name) //입력 받았을때만 post요청 날립니다.
 	{
 		await postData('/api/addRepAttr', { name }).then((data) => 
 		{
 			console.log(data); // JSON data parsed by `data.json()` call
+
 		});
 	}
 };
 
-const addRepJoinKey = async () => {
+const addRepJoinKey = async (e) => {
 	const name = prompt ("추가하고자하는 대표 결합키를 입력하세요");
-	console.log(name);
-	if (name)
+	if (!name)
+		e.target.value = "-";
+	if (name) //입력 받았을때만 post 요청 날립니다.
 	{
 		await postData('/api/addRepJoinKey', { name }).then((data) => 
 		{
@@ -29,9 +32,9 @@ const addRepJoinKey = async () => {
 const handleListener = async (e) => {
 	try {
 		if (e.target.value == "addRepAttr")
-			await addRepAttr();
+			await addRepAttr(e);
 		else if (e.target.value == "addRepJoinKey")
-			await addRepJoinKey();
+			await addRepJoinKey(e);
 	} catch (e) {
 		console.log(e);
 	}
