52c2503a0a0af5a3e8dc7a2317e277279f463df6
myukang <myunghwan0421@gmail.com>
2022-11-28 02:42:26 +0900

    DESIGN:공백 추가

--- src/routers/A/domainScanRouter.js
+++ src/routers/A/domainScanRouter.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import { checkLoginMiddleware } from '../../middlewares';
 import { getDomainScan, getDomainScanResult, saveMappingData } from '../../Controller/A/domainScanController';
+import {addRepAttr, addRepJoinKey} from '../../Controller/Common/apiController';
 
 const domainScanRouter = express.Router();
 
-domainScanRouter.get("/domain-scan",checkLoginMiddleware, getDomainScan);
+domainScanRouter.get("/domain-scan", checkLoginMiddleware, getDomainScan);
 
 domainScanRouter.get("/domain-scan/:tableName([0-9A-Za-zㄱ-ㅎ-_]+)", checkLoginMiddleware, getDomainScanResult);
 
 domainScanRouter.post("/domain-scan/:tableName([0-9A-Za-zㄱ-ㅎ-_]+)/save", checkLoginMiddleware, saveMappingData)
+
+domainScanRouter.post("/api/addRepAttr", addRepAttr);
+domainScanRouter.post("/api/addRepJoinKey", addRepJoinKey);
 
 export default domainScanRouter;
