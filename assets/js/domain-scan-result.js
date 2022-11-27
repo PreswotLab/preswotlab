bd85f0e91eafcc86cb9a708a77a64ed82d99aae4
myukang <myunghwan0421@gmail.com>
2022-11-28 03:18:22 +0900

    FEAT: if~else 분기문으로 변경

--- assets/js/domain-scan-result.js
+++ assets/js/domain-scan-result.js
@@ -6,12 +6,12 @@ const addRepAttr = async (e) => {
 	const name = prompt ("추가하고자하는 대표 속성을 입력하세요");
 	if (!name)
 		e.target.value = "-";
-	if (name) //입력 받았을때만 post요청 날립니다.
+	else //입력 받았을때만 post요청 날립니다.
 	{
 		await postData('/api/addRepAttr', { name }).then((data) => 
 		{
 			console.log(data); // JSON data parsed by `data.json()` call
-
+		e.target.value = name;
 		});
 	}
 };
@@ -20,11 +20,12 @@ const addRepJoinKey = async (e) => {
 	const name = prompt ("추가하고자하는 대표 결합키를 입력하세요");
 	if (!name)
 		e.target.value = "-";
-	if (name) //입력 받았을때만 post 요청 날립니다.
+	else //입력 받았을때만 post 요청 날립니다.
 	{
 		await postData('/api/addRepJoinKey', { name }).then((data) => 
 		{
 			console.log(data); // JSON data parsed by `data.json()` call
+		e.target.value = name;
 		});
 	}
 };
