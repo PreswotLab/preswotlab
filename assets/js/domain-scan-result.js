277313e05e53bfe5c858f5b9988690d1615f20ea
myukang <myunghwan0421@gmail.com>
2022-11-28 11:20:33 +0900

    FEAT:대표속성 추가, 대표결합키 추가기능
    대표 속성 및 대표 결합키 추가시, fetch를 통해 성공적으로 추가됐는지
    못했는지 data를 받아온다.
    data.status == 1인 경우 성공했으므로, 현재 DOM 에 대표 속성 및
    결합키들을 select의 option에 넣어준다.
    data.status == 0인 경우 실패

--- assets/js/domain-scan-result.js
+++ assets/js/domain-scan-result.js
@@ -11,11 +11,23 @@ const addRepAttr = async (e) => {
 		await postData('/api/addRepAttr', { name }).then((data) => 
 		{
 			console.log(data); // JSON data parsed by `data.json()` call
+			if (data.status == 1)
+			{
+				const repAttrSelects = document.getElementsByClassName("repAttr");
+				for (let i = 0; i < repAttrSelects.length; i++)
+				{
+					let option = document.createElement("option");
+					option.innerHTML = name;
+					option.value = name;
+					console.log(option);
+					repAttrSelects[i].add(option, 1);
+				}
+			}
 		});
 		e.target.value = name;
 	}
-};
-
+}; 
+ 
 const addRepJoinKey = async (e) => {
 	const name = prompt ("추가하고자하는 대표 결합키를 입력하세요");
 	if (!name)
@@ -25,6 +37,18 @@ const addRepJoinKey = async (e) => {
 		await postData('/api/addRepJoinKey', { name }).then((data) => 
 		{
 			console.log(data); // JSON data parsed by `data.json()` call
+			if (data.status == 1)
+			{
+				const repJoinKeySelects = document.getElementsByClassName("repJoinKey");
+				for (let i = 0; i < repJoinKeySelects.length; i++)
+				{
+					let option = document.createElement("option");
+					option.innerHTML = name;
+					option.value = name;
+					console.log(option);
+					repJoinKeySelects[i].add(option, 1);
+				}
+			}
 		});
 		e.target.value = name;
 	}
