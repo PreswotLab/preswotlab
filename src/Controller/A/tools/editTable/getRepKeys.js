5a2d3d4f778d48e4acb244ccab2dccd7de295f5d
myukang <myunghwan0421@gmail.com>
2022-11-29 21:17:29 +0900

    FEAT: 대표속성, 대표키 가져오는 함수 모듈화

---
+++ src/Controller/A/tools/editTable/getRepKeys.js
@@ -0,0 +1,12 @@
+import getServerLoginInfo from "../../../Common/tools/user/getServerLoginInfo"
+import dbConnectQuery from "../../../Common/tools/user/dBConnectQuery";
+
+export const getRepKeys = async () => {
+	const serverLoginInfo = getServerLoginInfo();
+	const repKeyResult = await dbConnectQuery(serverLoginInfo,
+	`
+	SELECT rkey_name
+	FROM tb_rep_key;
+	`);
+	return (repKeyResult);
+}
