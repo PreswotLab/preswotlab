5c6f169d5fac2495c3ba20d4d56f775dd9a32b86
myukang <myunghwan0421@gmail.com>
2022-11-29 21:20:49 +0900

    FEAT: editTable 컨트롤러 완성,

--- src/Controller/A/editTableController.js
+++ src/Controller/A/editTableController.js
@@ -1,4 +1,8 @@
+import getServerLoginInfo from "../Common/tools/user/getServerLoginInfo";
 import {getTableNamesAndScanyn} from "./tools/domainScan/getTableNamesAndScanyn";
+import {getRepAttrs} from "./tools/editTable/getRepAttrs";
+import {getRepKeys} from "./tools/editTable/getRepKeys";
+import dbConnectQuery from "../Common/tools/user/dBConnectQuery";
 
 export const getEditTableHome = async (req, res) => {
 	const tbNameScanYn = await getTableNamesAndScanyn(req.session.loginInfo.user_seq);
@@ -7,12 +11,59 @@ export const getEditTableHome = async (req, res) => {
 }
 
 export const getEditTableRows = async (req, res) => {
-	const { loginInfo } = req.session;
 	const { tableName } = req.params;
+	const { user_seq } = req.session.loginInfo;
+	const serverLoginInfo = getServerLoginInfo();
+	console.log("tableName: ", tableName);
+	console.log("userseq : ", user_seq);
 
-
-
-	res.render('edit-table-view', {});
+	try {
+		const result = await dbConnectQuery(serverLoginInfo, 
+			`
+			SELECT
+			a.attr_name, 
+			a.attr_type, 
+			a.d_type, 
+			a.null_num, 
+			a.null_num / sc.row_num AS null_portion, 
+			diff_num, 
+			a.special_num, 
+			a.special_num / sc.row_num AS special_portion, 
+			a.max_value, 
+			a.min_value, 
+			a.zero_num, 
+			a.zero_num / sc.row_num AS zero_portion, 
+			a.key_candidate, 
+			ra.rattr_name, 
+			rk.rkey_name 
+			FROM tb_scan sc, tb_attribute a, tb_mapping m, tb_rep_key rk, tb_rep_attribute ra
+			WHERE sc.user_seq = ${user_seq} 
+			AND sc.table_name = '${tableName}'
+			AND sc.table_seq = a.table_seq 
+			AND a.attr_seq = m.attr_seq 
+			AND m.rkey_seq = rk.rkey_seq 
+			AND a.rattr_seq = ra.rattr_seq;
+			`);
+		const numericResult = [];
+		const categoryResult = [];
+		for (let i = 0; i < result.length; i++)
+		{
+			if (result[i]['attr_type'] == 'C')
+				categoryResult.push(result[i]);
+			else
+				numericResult.push(result[i]);
+		}
+		const repAttrJoinKey = {
+			repAttrArray : await getRepAttrs(),
+			repKeyArray : await getRepKeys()
+		};
+		console.log(result);
+		res.render('edit-table-view', { tableName, numericResult, categoryResult, repAttrJoinKey });
+	} catch (e)
+	{
+		console.log(e.message);
+		res.redirect('/logout');
+	}
 }
 
 /*
