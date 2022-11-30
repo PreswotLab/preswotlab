8566d4d98ba561430a2a18e06e42b6bc05d6363c
myukang <myunghwan0421@gmail.com>
2022-11-30 19:41:45 +0900

    FEAT:editTable 태영님 쿼리 저장

--- src/Controller/A/editTableController.js
+++ src/Controller/A/editTableController.js
@@ -41,6 +41,47 @@ export const getEditTableRows = async (req, res) => {
 			AND m.rkey_seq = rk.rkey_seq 
 			AND a.rattr_seq = ra.rattr_seq;
 			`);
+// SELECT
+//     a.attr_seq,
+//     a.table_seq,
+//     sc.table_name,
+//     a.attr_name,
+//     a.attr_type,
+//     a.d_type,
+//     a.null_num,
+//     a.null_num / sc.row_num AS null_portion,
+//     a.diff_num,
+//     a.max_value,
+//     a.min_value,
+//     a.zero_num,
+//     a.zero_num / sc.row_num AS zero_portion,
+//     a.special_num,
+//     a.special_num / sc.row_num AS special_portion,
+//     a.rattr_seq,
+//     a.key_candidate,
+//     ra.rattr_name,
+//     tb.mapping_seq,
+//     tb.rkey_seq,
+//     tb.rkey_name
+// FROM tb_attribute a
+//     INNER JOIN tb_scan sc ON a.table_seq = sc.table_seq
+//     LEFT OUTER JOIN tb_rep_attribute ra ON a.rattr_seq = ra.rattr_seq # rattr_seq이 없을 수 있기 때문에 LEFT OUTER JOIN
+//     LEFT OUTER JOIN # mapping이 안되어 있을 수 있기 때문에 LEFT OUTER JOIN
+//         (SELECT
+//              m.mapping_seq,
+//              m.rkey_seq,
+//              m.attr_seq,
+//              m.table_seq,
+//              m.chg_yn,
+//              rk.rkey_name
+//         FROM tb_mapping m
+//         INNER JOIN tb_rep_key rk ON m.rkey_seq = rk.rkey_seq
+//         WHERE m.chg_yn = 'N' # mapping은 단 하나만 존재해야 한다 / mapping 변경되면 'Y'로 바꿔줘야 함
+//         ) tb ON a.attr_seq = tb.attr_seq and a.table_seq = tb.table_seq
+// WHERE
+//     sc.user_seq = 16 and # 파라미터 이용
+//     sc.table_name = 'clients' and # 파라미터 이용
+//     sc.scan_yn = 'Y' # scan 된 것만 가져온다.
 		const numericResult = [];
 		const categoryResult = [];
 		for (let i = 0; i < result.length; i++)
@@ -65,11 +106,23 @@ export const getEditTableRows = async (req, res) => {
 /*
  * 속성 삭제
  * req.session.loginInfo로 사용자 DB에 쿼리 날려야한다.
+ * serverLoginInfo로 tb_attribute와 tb_mapping에 관련 속성을 삭제해야한다.
  * */
-export const deleteAttr = async (req, res) => {
 
+export const deleteAttr = async (req, res) => {
+	try {
+		console.log(req.body);
+		res.send({ status : 1 });
+	} catch (e) {
+		res.send({ status : 0 });
+	}
 };
 
 export const modAttr = async (req, res) => {
-
+	try {
+		console.log(req.body);
+		res.send({ status : 1 });
+	} catch (e) {
+		res.send({ status : 0 });
+	}
 }
