d556962abbdffe8b50ef059b4f16bb0722992c6d
myukang <myunghwan0421@gmail.com>
2022-12-01 14:44:30 +0900

    FEAT:console.log 삭제 및 table_seq 가져오는 쿼리 수정

--- src/Controller/A/tools/domainScan/SaveMappingObj.js
+++ src/Controller/A/tools/domainScan/SaveMappingObj.js
@@ -24,10 +24,10 @@ export class SaveMapping
 
 	async init()
 	{
-		await this.#setTableSeq();
-		await this.#setAttrSeqObj();
-		await this.#setRattrSeqObj();
-		await this.#setRkeySeqObj();
+		await this.#setTableSeq(); //user_seq를 가진 table_seq를 tb_scan에서 가져온다.
+		await this.#setAttrSeqObj();//현재 table_seq를 기준으로 {attr_name : attr_seq, ... }로 객체를 생성한다.
+		await this.#setRattrSeqObj();//서버에 저장된 모든 {rattr_name : rattr_seq, ... }로 객체를 생성한다.
+		await this.#setRkeySeqObj();//서버에 저장된 모든 {rkey_name : rkey_seq, ...}로 객체를 생성한다.
 	}
 
 	async save(mappingRep)
@@ -56,16 +56,31 @@ export class SaveMapping
 
 	async #saveRepJoinKeyMapping(attrName, mapAttrName)
 	{
+		/*
+		 * tb_mapping에서 기존 정보를 업데이트해야한다.
+		 *
+		 * */
 		const attr_seq = this.#attrSeqObj[attrName];
 		const rkey_seq = this.#rKeySeqObj[mapAttrName];
 		await dbConnectQuery(this.#serverInfo,
 		`
 			UPDATE tb_mapping
-			SET rkey_seq = ${rkey_seq}, chg_yn = 'Y'
+			SET chg_yn = 'Y' #기존에 현재 테이블과 속성을 매핑하는 튜플을 'Y'로 변경.
 			WHERE 
-			attr_seq = ${attr_seq} 
+			attr_seq = ${attr_seq}
 			AND table_seq = ${this.#tableSeq};
-			;
+		`)
+		await dbConnectQuery(this.#serverInfo,
+		`
+			INSERT tb_mapping ( #새로 삽입
+				rkey_seq, 
+				attr_seq,
+				table_seq
+			) VALUES (
+				${rkey_seq},
+				${attr_seq},
+				${this.#tableSeq}
+			);
 		`)
 	}
 
@@ -73,14 +88,12 @@ export class SaveMapping
 	{
 		const result = await dbConnectQuery(this.#serverInfo,
 		`
-			SELECT table_seq, table_name
+			SELECT table_seq
 			FROM tb_scan
 			WHERE table_name = '${this.#tableName}'
 			AND user_seq = ${this.#userSeq};
 		`);
 		this.#tableSeq = result[0]['table_seq'];
-		console.log(result);
-		console.log('tableSeq:', this.#tableSeq);
 	}
 
 	async #setAttrSeqObj()
@@ -91,15 +104,11 @@ export class SaveMapping
 			FROM tb_attribute
 			WHERE table_seq = ${this.#tableSeq};
 		`)
-		console.log(
-			"attrSeq, attrName : ", result
-		)
 		for (let i = 0; i < result.length; i++)
 		{
 			let attrName = result[i]['attr_name'];
 			this.#attrSeqObj[attrName] = result[i]['attr_seq'];
 		}
-		console.log("attr seq obj: ",this.#attrSeqObj);
 	}
 
 	async #setRattrSeqObj()
