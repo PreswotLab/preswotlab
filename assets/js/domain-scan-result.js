01b3ca6c15994a411bc945ff89f553eaae98e04c
myukang <myunghwan0421@gmail.com>
2022-12-09 13:59:22 +0900

    FEAT: boxplot update, getData fetch 생성

--- assets/js/domain-scan-result.js
+++ assets/js/domain-scan-result.js
@@ -1,8 +1,37 @@
 import repSelectListener from "./modules/repSelectListner.js";
+import getData from "./modules/getData.js";
+import {makeChartOption} from "./modules/makeChartOption.js";
+import {renderingBoxplot} from "./modules/renderingBoxplot.js";
 
 const selectTags = document.getElementsByClassName("repSelect");
+const container = document.getElementById('boxplot_container')
+const tableName = document.getElementById('tableName').innerHTML;
 
 for (let i = 0; i < selectTags.length; i++)
 {
 	selectTags[i].addEventListener('change', repSelectListener);
 };
+
+for (let i = 0; i < selectTags.length; i++)
+{
+	selectTags[i].addEventListener('change', repSelectListener);
+};
+
+try {
+	let childNode;
+	let option;
+	const response = await getData(`/api/boxplot/${tableName}`);
+
+	if (response.status == 0)
+		throw(Error);
+	for (let i = 0; i < response.data.length; i++)
+	{
+		childNode = document.createElement("div");
+		container.appendChild(childNode);
+		option = makeChartOption(response.data[i]);
+		renderingBoxplot(childNode, option);
+	}
+} catch (e) {
+	console.log(e.message);
+}
+
